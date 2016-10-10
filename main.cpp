60bd5228086f5be20d032154b583159f8e81459c
Yuriy Kurnosov <kurnosov98@yandex.ru>
2016-10-10 13:03:16 +0300

    Add number og input arguments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+class You
+{
+private:
+    int group;
+public:
+    You(): group(0){}
+    You(int group): group(group){}
+    void vivod()
+    {cout << "group: " << group<<endl;}
+};
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello, World!\n";
-
+    You i(221);
+    i.vivod();
+    cout<<"Hello, World\n";
     return 0;
 }
